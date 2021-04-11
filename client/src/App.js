c0d283efe874e2de9ae1380489ddbb25cc9ccc0a
Phil Everitt <phileveritt@mac.com>
2021-04-11 14:52:07 -0700

    Revert "retry"
    This reverts commit fc20bcdc218de5c5ee308604b2e2cc202e5b4e86.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link} from  'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
@@ -11,21 +10,14 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">Other page here</Link>
+          <div>
+            <Route exact path="/" component={Fib}/>
+            <Route path="/otherpage" component={OtherPage}/>
+          </div>
         </header>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
-        </div>
       </div>
     </Router>
   );
