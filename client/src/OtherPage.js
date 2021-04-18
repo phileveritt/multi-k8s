956e9903a522b8db8aaab328804c9c20c1b81d2c
Phil Everitt <phileveritt@mac.com>
2021-04-18 11:58:47 -0700

    sync222

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const def = () => {
-    return (
-        <div>
-            I'm some other page!
-            <Link to="/">Go back home</Link>
-        </div>
-    );
+export default () => {
+  return (
+    <div>
+      Im some other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
 };
-
-export default def;
