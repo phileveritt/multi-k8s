1a2bd7e81fa890d6e74e483007d23d2fe58fe42e
Phil Everitt <phileveritt@mac.com>
2021-05-12 16:14:06 -0700

    revert back to phileveritt docker id

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t peveritt/multi-client:latest -t peveritt/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t peveritt/multi-server:latest -t peveritt/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t peveritt/multi-worker:latest -t peveritt/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push peveritt/multi-client:latest
-docker push peveritt/multi-server:latest
-docker push peveritt/multi-worker:latest
-docker push peveritt/multi-client:$SHA
-docker push peveritt/multi-server:$SHA
-docker push peveritt/multi-worker:$SHA
+docker build -t phileveritt/multi-client:latest -t phileveritt/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t phileveritt/multi-server:latest -t phileveritt/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t phileveritt/multi-worker:latest -t phileveritt/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push phileveritt/multi-client:latest
+docker push phileveritt/multi-server:latest
+docker push phileveritt/multi-worker:latest
+docker push phileveritt/multi-client:$SHA
+docker push phileveritt/multi-server:$SHA
+docker push phileveritt/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/service-deployment server=peveritt/multi-server:$SHA
-kubectl set image deployments/client-deployment client=peveritt/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=peveritt/multi-worker:$SHA
+kubectl set image deployments/service-deployment server=phileveritt/multi-server:$SHA
+kubectl set image deployments/client-deployment client=phileveritt/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=phileveritt/multi-worker:$SHA
